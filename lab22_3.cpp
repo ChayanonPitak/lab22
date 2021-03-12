5d84e9349dd6412174de331dbe08ed0bd3c464cd
IkaWaAyuMu <pungpond4450@gmail.com>
2021-03-12 15:59:25 +0700

    Commit?

--- lab22_3.cpp
+++ lab22_3.cpp
@@ -1,8 +1,15 @@
 #include<iostream>
+#include<set>
 
 using namespace std;
 
 //Write function count() here.
+template <class T> int count(T data[], int count)
+{
+	set<T> UniqueMem;
+	for (int i=0 ; i<count ; i++) UniqueMem.insert(data[i]);
+	return UniqueMem.size();
+}
 
 int main(){	
 	int data[] = {1,2,4,5,4,8,2,1,2,4,6,1,4,4,4,2,1,0,12}; 
